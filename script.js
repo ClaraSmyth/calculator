fb9fc7499299afeb9be65fa52ed1b866f065dd6c
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-05 21:15:22 +0100

    Added a way to call the operators with 2 numbers

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
-console.log(add(3, 3))
+function operate(a, b, operator) {
+    switch (operator) {
+        case 'add':
+            return add(a, b);
+            break;
+        case 'subtract':
+            return subtract(a, b);
+            break;
+        case 'multiply':
+            return multiply(a, b);
+            break;
+        case 'divide':
+            return divide(a, b);
+            break;
+    }
+}
+
+console.log(operate(3, 3, 'add'))
