9a22705b568c5acabe2eb009653c3339f2a07c71
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-05 21:16:14 +0100

    Cleaned up the code

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
-
 function operate(a, b, operator) {
     switch (operator) {
         case 'add':
-            return add(a, b);
+            return a + b;
             break;
         case 'subtract':
-            return subtract(a, b);
+            return a - b;
             break;
         case 'multiply':
-            return multiply(a, b);
+            return a * b;
             break;
         case 'divide':
-            return divide(a, b);
+            return a / b;
             break;
     }
 }
