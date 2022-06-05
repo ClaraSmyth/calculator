3e4bf30eed2e817fffd72dec31c0685e6d464ef1
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-05 23:15:47 +0100

    Operator buttons are now working and updating

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ const buttons = document.querySelectorAll('button')
 const current = document.querySelector('.current')
 const previous = document.querySelector('.previous')
 let currentValue = 0;
+let previousValue = 0;
+let operator = 0;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         console.log()
         if (button.id === 'number') {
             current.innerText = current.innerText + `${button.innerText}`;
-            currentValue = current.innerText + `${button.innerText}`;
-            console.log(currentValue)
+            currentValue = current.innerText;
+        } else if (button.id === 'operator') {
+            previousValue = currentValue;
+            currentValue = 0;
+            previous.innerText = current.innerText + `${button.innerText}`;
+            current.innerText = null;
+            operator = button.innerText;
+            console.log(currentValue, previousValue, operator)
         }
     })
 })
